ec0218e8ac2790273c5041331fcb5b36e1e3eb4d
Gunja's Github <gunjagurung@gunjas-imac.local>
2018-08-04 14:42:33 -0400

    Scroll view programmatically

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
-class ImageViewController: UIViewController {
+class ImageViewController: UIViewController, UIScrollViewDelegate {
 
     var imageURL: URL? {
         didSet {
-            imageView.image = nil
+            image = nil
             if view.window != nil {
                 fetchImage()
             }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit() //Make yourself your intrinsic size
+            scrollView.contentSize = imageView.frame.size
         }
     }
 
@@ -33,13 +44,26 @@ class ImageViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var imageView: UIImageView!
+    var imageView = UIImageView()
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
     
     private func fetchImage() {
         if let url = imageURL {
             let urlContents = try? Data(contentsOf: url)
             if let imageData = urlContents {
-                imageView.image = UIImage(data: imageData)
+                self.image = UIImage(data: imageData)
             }
         }
     }
